e560cb3045499f714928b1b9fdad4503a56d30b7
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2026-09-20 13:02:22 -0600

    fix blocked hosts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        allowedHosts: true,
-    },
-    preview: {
-        host: '0.0.0.0',
-        port: 4173,
-        allowedHosts: ['flownutrition.onrender.com', 'localhost', 'flownutrition.com.mx'],
-    },
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT || 4173),
+    allowedHosts: true,
+  },
 })
