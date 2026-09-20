488113d1e0ab3de59c83627c9926ec491dfa8e96
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2026-09-20 12:36:40 -0600

    fix dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 4173
-  }
+    plugins: [react()],
+    server: {
+        allowedHosts: true
+    }
 })
